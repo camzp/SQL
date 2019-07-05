9e5f85a07be63d4b85e63953334055add94ebc99
Camila Camilo <camilacpbc@gmail.com>
2019-07-05 15:08:39 -0300

    Update select_from_world.sql

--- select_from_world.sql
+++ select_from_world.sql
@@ -1,9 +1,76 @@
+-- https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial --
+
 ---
 
-(1) Show the name for the countries that have a population of at least 200 million. 
+(2) Show the name for the countries that have a population of at least 200 million. 
     200 million is 200000000, there are eight zeros.
 ---
 
 SELECT name FROM world
 WHERE population = 64105700
+
+---
+
+(3) Give the name and the per capita GDP for those countries with a population of at least 200 million.
+
+---
+
+SELECT name, gdp/population FROM world
+WHERE population > 200000000;
+
+--
+
+(4) Show the name and population in millions for the countries of the continent 'South America'. 
+    Divide the population by 1000000 to get population in millions.
+
+--
+
+SELECT name, population/1000000 FROM world
+WHERE continent = 'South America'
+
+--
+
+(5) Show the name and population for France, Germany, Italy
+
+--
+
+SELECT name, population FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+--
+
+(6) Show the countries which have a name that includes the word 'United'
+
+--
+
+SELECT name FROM world
+WHERE name LIKE'United%'
+
+--
+
+(7) Show the countries that are big by area or big by population. Show name, population and area.
+
+--
+
+SELECT name, population, area FROM world
+WHERE area > 3000000 OR population > 250000000
+
+--
+
+(8) Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. 
+    Show name, population and area.
+--
+
+SELECT name, population, area FROM world
+WHERE area > 3000000 XOR population > 250000000
+
+--
+
+(9)
+
+--
+
+SELECT name, ROUND(population/1000000  , 2), ROUND(GDP/1000000000 ,2) FROM world
+WHERE continent = 'South America';
+
 
