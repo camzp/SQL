4d3d93f2e759c0c322b3ce78905de7a7a22bdb97
Camila Camilo <camilacpbc@gmail.com>
2019-07-05 16:36:34 -0300

    Update select_from_world.sql

--- select_from_world.sql
+++ select_from_world.sql
@@ -73,4 +73,49 @@ WHERE area > 3000000 XOR population > 250000000
 SELECT name, ROUND(population/1000000  , 2), ROUND(GDP/1000000000 ,2) FROM world
 WHERE continent = 'South America';
 
+--  
+
+(10) Show the name and per-capita GDP for those countries with a GDP of at least one trillion
+     (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+--
+
+SELECT name,ROUND((gdp/population), -3) AS PerCapitaGDP  from world
+WHERE GDP >= 1000000000000
+
+--
+
+(11) Show the name and capital where the name and the capital have the same number of characters.
+
+--
+
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) =LENGTH(capital)
+
+--
+
+(12) Show the name and the capital where the first letters of each match. 
+    Dont include countries where the name and the capital are the same word.
+
+--
+
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT (capital,1) AND name <> capital
+
+--
+
+(13) Find the country that has all the vowels and no spaces in its name.
+
+--
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%' 
+AND name LIKE '%e%' 
+AND name LIKE '%i%' 
+AND name LIKE '%o%'
+AND name LIKE '%u%' 
+AND name NOT LIKE '% %'
 
