71b6932c8d0be8916f4a8118a72c563a38753b8c
Camila Camilo <camilacpbc@gmail.com>
2019-07-05 16:04:35 -0300

    Update zoo_select_names.sql

--- zoo_select_names.sql
+++ zoo_select_names.sql
@@ -1,90 +1,138 @@
+-- https://sqlzoo.net/wiki/SELECT_names --
+
 --
 
 (1)  You can use WHERE name LIKE 'B%' to find the countries that start with "B".
 
 --
 
---
-
-(2)
-
---
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
 
 --
 
-(3)
+(2) Find the countries that end with y
 
 --
 
---
-
-(4)
-
---
+SELECT name FROM world
+  WHERE name LIKE '%Y'
 
 --
 
-(5)
+(3) Find the countries that contain the letter x
 
 --
 
---
-
-(6)
-
---
+SELECT name FROM world
+  WHERE name LIKE '%X%'
 
 --
 
-(7)
+(4) Iceland, Switzerland end with land - but are there others? Find the countries that end with land
 
 --
 
---
-
-(8)
-
---
+SELECT name FROM world
+  WHERE name LIKE '%land'
 
 --
 
-(9)
+(5) Find the countries that start with C and end with ia
 
 --
 
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+  
 --
 
-(10)
-
---
-
---
-
-(11)
+(6) Find the country that has oo in the name
 
 --
 
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+  
 --
 
-(12)
+(7) Find the countries that have three or more a in the name
 
 --
-
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
 --
 
-(13)
-
---
-
---
-
-(14)
-
---
+(8) Find the countries that have "t" as the second character.
 
 --
 
-(15)
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
 
 --
 
+(9) Find the countries that have two "o" characters separated by two others.
+
+--
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+--
+
+(10) Find the countries that have exactly four characters.
+
+--
+
+SELECT name FROM world
+ WHERE name LIKE '____'
+ 
+--
+
+(11) Find the country where the name is the capital city.
+
+--
+
+SELECT name
+  FROM world
+ WHERE name LIKE capital
+ 
+--
+
+(12) Find the country where the capital is the country plus "City".
+
+--
+
+SELECT name
+  FROM world
+WHERE capital = concat(name, ' City');
+
+--
+
+(13) Find the capital and the name where the capital includes the name of the country.
+
+--
+
+SELECT capital, name
+  FROM world
+WHERE capital LIKE concat('%', name, '%');
+
+--
+
+(14) Find the capital and the name where the capital is an extension of name of the country.
+
+--
+
+SELECT capital, name from world
+WHERE capital LIKE concat (name,'%') AND capital > name;
+
+--
+
+(15) Show the name and the extension where the capital is an extension of name of the country.
+
+--
+
+SELECT name , REPLACE(capital, name , '') AS ext FROM world
+WHERE capital LIKE concat(name, '_%');
