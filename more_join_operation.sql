faa0a0d97a09ba46fdc492dd1b3b562672ec13c1
Camila Camilo <camilacpbc@gmail.com>
2019-07-08 10:47:19 -0300

    Update more_join_operation.sql

--- more_join_operation.sql
+++ more_join_operation.sql
@@ -1,1 +1,121 @@
 -- https://sqlzoo.net/wiki/More_JOIN_operations --
+
+--
+
+(1) List the films where the yr is 1962 [Show id, title]
+
+--
+
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+ 
+--
+
+(2) Give year of 'Citizen Kane'.
+
+--
+
+SELECT yr FROM movie
+WHERE title = 'Citizen Kane'
+
+--
+    List all of the Star Trek movies, include the id, title and yr 
+(3) Order results by year.
+    (all of these movies include the words Star Trek in the title). 
+--
+
+SELECT id, title, yr FROM movie
+WHERE title LIKE '%Star Trek%'
+
+--
+
+(4) What id number does the actor 'Glenn Close' have?
+
+--
+
+SELECT id from actor
+WHERE name = 'Glenn Close'
+
+--
+
+(5) What is the id of the film 'Casablanca'
+
+--
+
+SELECT id FROM movie
+WHERE title = 'Casablanca'
+
+--
+
+(6) Obtain the cast list for 'Casablanca'.
+
+--
+
+SELECT name FROM movie
+JOIN casting ON (movieid = movie.id) 
+JOIN actor ON (actorid = actor.id)
+WHERE movieid = 11768
+
+--
+
+(7) Obtain the cast list for the film 'Alien'
+
+--
+
+
+--
+
+(8) Harrison Ford movies
+
+--
+
+
+--
+
+(9) Harrison Ford as a supporting actor
+
+--
+
+
+--
+
+(10) List the films together with the leading star for all 1962 films.
+
+--
+
+
+--
+     Which were the busiest years for 'John Travolta', 
+(11) show the year and the number of movies he made each year 
+     for any year in which he made more than 2 movies.
+--
+
+
+--
+
+(12) List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+--
+
+
+--
+
+(13) Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles
+
+--
+
+
+--
+
+(14) List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
+--
+
+
+--
+
+(15) List all the people who have worked with 'Art Garfunkel'.
+
+--
+
